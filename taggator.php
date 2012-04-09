fcfe82c23835d9d9d992d35888f97edb14616ebf
angrybyte <angrybyte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-09 19:39:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/taggator/trunk@529227 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- taggator.php
+++ taggator.php
@@ -3,19 +3,19 @@
 /**
  * @package TagGator
  * @author PsMan
- * @version 1.31
+ * @version 1.33
  */
 /*
 Plugin Name: TagGator
 Plugin URI: http://angrybyte.com/wordpress-plugins/taggator/
 Description: TagGator automatically tags posts so that you dont have to! Just set some keywords, taggator will look them up and tag matching posts automagically. A pro version is also available! 
 Author: PsMan
-Version: 1.31
+Version: 1.33
 Author URI: http://angrybyte.com
 */
 
 add_action('admin_menu', 'taggatorfreemenu');
-//old desc TagGator is an auto tagging plugin, provide the plugin with some keywords, it will convert these keywords to tags and automatically adds this tag to all posts containing these keywords. 
+//old desc TagGator is an auto tagging plugin, provide the plugin with some keywords, it will convert these keywords to tags and automatically adds this tag to all posts containing these keywords.
 add_option("taggatorcs", '1', 'Case sensitivity for taggator?', 'yes');
 add_option("taggatormhw", '1', 'Match whole words?', 'yes');
 add_option("taggatortags", '', 'Taggator tags', 'yes');
@@ -55,70 +55,68 @@ function taggatorfree_plugin_options()
         update_option('taggatortags', $tagz);
         $tagz = str_ireplace("\n", ",", $tagz);
         $tagz = explode(',', $tagz);
+
         global $post;
         global $wpdb;
         $table_name = $wpdb->prefix . "posts";
         $pfx = $wpdb->prefix;
         //$myposts = get_posts('numberposts=-1'); Solve memory issues
         $myposts = $wpdb->get_col("SELECT id FROM `{$pfx}posts` WHERE `post_status` = 'publish'");
+
         $nofmatchs = 0;
-        foreach ($tagz as $atag)
+
+
+        foreach ($myposts as $post)
         {
 
-            foreach ($myposts as $post)
+            $ido = $post;
+            $zepost = get_post($post);
+            $aa = " " . $zepost->post_content;
+
+            foreach ($tagz as $atag)
             {
 
-
-                $ido = $post;
-                $zepost = get_post($post);
-                $aa = " " . $zepost->post_content;
-
-
+$atag=trim($atag);
                 if ($mhw && ((!$cst && (preg_match("/\b$atag\b/iu", strtolower($aa)))) || ($cst &&
                     (preg_match("/\b$atag\b/u", ($aa))))) || !$mhw && (!$cst && (strpos(strtolower($aa),
                     strtolower($atag))) || ($cst && (strpos(($aa), ($atag))))))
                 {
+
                     $nofmatchs = $nofmatchs + 1;
                     $pst["name"] = $atag;
                     $slugg = strtolower(str_replace(" ", "-", $atag));
                     $pst["slug"] = $slugg;
                     $nma = $atag;
-                    $already = $wpdb->get_var("select term_id from {$pfx}terms where lcase(name) = lcase('$nma') limit 1");
+                    $already = $wpdb->get_var($wpdb->prepare("select term_id from {$pfx}terms where lcase(name) = lcase(%s) limit 1",$nma));
+                  
                     if (!$already)
                     {
+
                         $pst["name"] = $atag;
                         $slugg = strtolower(str_replace(" ", "-", $atag));
                         $pst["slug"] = $slugg;
                         wp_insert_term($atag, 'post_tag', $pst);
 
 
-                        $qry = "select `term_id` from " . $pfx . "terms where name = '$atag' limit 1;";
-                        $tagid = $wpdb->get_var($qry);
+                        $qry = "select `term_id` from  {$pfx}terms where name = %s limit 1;";
+                        //echo $wpdb->prepare($qry, $atag);
+                        $tagid = $wpdb->get_var($wpdb->prepare($qry, $atag));
                     } else
                     {
                         $tagid = $already;
 
                     }
 
-                    $qry = "SELECT term_taxonomy_id from " . $pfx . "term_taxonomy where term_id= $tagid limit 1;";
+                    $qry = "SELECT term_taxonomy_id from {$pfx}term_taxonomy where term_id= %d limit 1;";
 
 
-                    $taxid = $wpdb->get_var($qry);
+                    $taxid = $wpdb->get_var($wpdb->prepare($qry, $tagid));
 
 
-                    $qry = "
-INSERT INTO `" . $pfx . "term_relationships` (
+                    $wpdb->query($wpdb->prepare("INSERT INTO `{$pfx}term_relationships` (
 `object_id` ,
 `term_taxonomy_id` ,
-`term_order` 
-)
-VALUES (
-'$ido', '$taxid', '0'
-);
-";
-
-
-                    $wpdb->query($qry);
+`term_order` )VALUES (%d, %d, '0');", $ido, $taxid));
 
                 }
             }
@@ -149,7 +147,7 @@ VALUES (
 <p style="color:red;">$txtmat</p> 
 
 EOST;
-
+    
     echo "<table><tr><td width='80%'>";
     echo freetaggatorboxer("Tag list", "<Form method ='post' action='$serv' ><textarea  name='xx'  wrap='soft' cols='80' rows='10'>$oldtags</textarea><br />Separate tags with commas, TagGator will scan for these tags in your post contents then, create and apply the tag if it's found. <br /> <input type='checkbox' title = 'Case Sensetive?' name='cst' $chkd />Case Sensetive? <br /> <input type='checkbox' name='mhw' $chkd2 />Match whole words only? <br /><input type='checkbox' disabled='disabled' title = 'Scan titles?' name='taggatortagtitle' $chkdtit />Scan post titles for keywords? (Pro Feature) <br /><input disabled='disabled' type='checkbox' title = 'Auto Tag ?' name='autotagg' disabled='disabled'  />Auto tag new posts? (Pro Feature)<br /><input type='submit' class='button-primary widget-control-save' value='Save & apply tags now' style='width:250px'> </form>");
 
